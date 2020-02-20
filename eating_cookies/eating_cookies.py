e162c8db8945711642d8993e800267dd8dc73f20
Martin Twum Mensah <martint.mensah@gmail.com>
2020-02-20 12:52:50 +0000

    chore: Attempt eating_cookies with fibonacci trick Still not working

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -3,14 +3,44 @@
 import sys
 
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+
+def eating_cookies(n, cache=None):
+    # set a list to contain the list of possible combinations
+    if n <= 0:
+        return 1
+    elif n == 1:
+        return 1
+    else:
+        return eating_cookies(n - 1) + eating_cookies(n - 2)
+    # pass
+
+
+print(eating_cookies(20))
+
+# Python program to display the Fibonacci sequence
+
+# def recur_fibo(n):
+#    if n <= 1:
+#        return n
+#    else:
+#        return(recur_fibo(n-1) + recur_fibo(n-2))
+
+# nterms = 10
+
+# # check if the number of terms is valid
+# if nterms <= 0:
+#    print("Plese enter a positive integer")
+# else:
+#    print("Fibonacci sequence:")
+#    for i in range(nterms):
+#        print(recur_fibo(i))
+
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_cookies = int(sys.argv[1])
+#     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+#   else:
+#     print('Usage: eating_cookies.py [num_cookies]')
